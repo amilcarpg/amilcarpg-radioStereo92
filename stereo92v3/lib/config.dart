36c6c08be84d5ad5e687f5e4d256b3ab62cf102a
amilcarpg <amilcarpg@hotmail.com>
2025-04-18 00:41:04 -0500

    version 1

--- stereo92v3/lib/config.dart
+++ stereo92v3/lib/config.dart
@@ -1,3 +1,5 @@
 class Config {
-  static const String streamUrl = 'http://server-23.stream-server.nl:8438/stream?type=http&nocache=640181';
+  //static const String streamUrl = 'http://server-23.stream-server.nl:8438/stream?type=http&nocache=640181';
+  static const String streamUrl =
+      'https://stream-172.zeno.fm/c49wbe2r1f8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJjNDl3YmUycjFmOHV2IiwiaG9zdCI6InN0cmVhbS0xNzIuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IkRCVVVRendBVEc2OS1DT0lfX0d0NVEiLCJpYXQiOjE3NDQ5NTE1NzcsImV4cCI6MTc0NDk1MTYzN30.MGbHRoexbRZDMMbQXU6NVn-hRjg9fpu1Psu0WUQXX7M';
 }
